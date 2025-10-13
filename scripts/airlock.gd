ab5e02449c2a960e42cada2a4a703d6db68de301
Taxalottle Syndicate <133176852+DragonEyeGames@users.noreply.github.com>
2025-10-13 10:21:48 -0400

    Fixed Airline

--- scripts/airlock.gd
+++ scripts/airlock.gd
@@ -13,20 +13,20 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if(not state=="sealed"):
-		$State.play("open")
+		$DoorController.play("open")
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if(not state=="sealed"):
 		GameManager.playerState="outside"
-		$State.play("shut")
+		$DoorController.play("close")
 
 
 func _on_airlock_body_entered(body: Node2D) -> void:
 	if(state==""):
 		state="sealing"
 		GameManager.playerState="inside"
-		$State.play("seal")
+		$DoorController.play("seal")
 		await get_tree().create_timer(3).timeout
 		state="sealed"
 
@@ -38,7 +38,7 @@ func _on_airlock_body_exited(body: Node2D) -> void:
 func _on_outer_body_entered(body: Node2D) -> void:
 	if(state=="sealed"):
 		state="opening"
-		$State.play("exit")
+		#$State.play("exit")
 		GameManager.playerState="outside"
 		await get_tree().create_timer(3).timeout
 		state=""

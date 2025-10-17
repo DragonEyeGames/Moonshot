ede8c392468e1d94cea7feeee7121206272e3ce3
Taxalottle Syndicate <133176852+DragonEyeGames@users.noreply.github.com>
2025-10-16 20:12:59 -0400

    Tape Spawning (WIP)
    Tape now spawns in when you click! Just gotta make it patch the leak.

--- scenes/tape.gd
+++ scenes/tape.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
-var currentLine
+var currentLine=null
 
 func _process(delta: float) -> void:
 	global_rotation_degrees=90
 	if(Input.is_action_just_pressed("Click") and visible):
 		currentLine = Line2D.new()
-		currentLine.add_point(GameManager.tapeHolder.to_local($TapeStart.global_position))
 		GameManager.tapeHolder.add_child(currentLine)
-		currentLine.add_point(GameManager.tapeHolder.to_local($TapeStart.global_position)*1.1)
+		currentLine.add_point(GameManager.tapeHolder.to_local(get_viewport().get_canvas_transform().affine_inverse() * ($TapeStart.global_position)))
+		currentLine.add_point(GameManager.tapeHolder.to_local(get_viewport().get_canvas_transform().affine_inverse() * ($TapeStart.global_position)))
+		currentLine.width=10
+		currentLine.z_index=1
+		currentLine.default_color=Color(0, 0, 0)
 		print(currentLine.get_point_position(0))
+		print(currentLine.get_point_position(1))
+	if(currentLine!=null):
+		currentLine.set_point_position(1, GameManager.tapeHolder.to_local(get_viewport().get_canvas_transform().affine_inverse() * ($TapeStart.global_position)))
+	if(Input.is_action_just_released("Click") and visible):
+		currentLine=null

fef8856d92bf52b50fd628afccd942e5ab544fc7
Taxalottle Syndicate <bthole41@gmail.com>
2025-10-20 17:05:32 -0400

    Cleaning Up (WIP)
    My code will be so sparkly after this

--- scripts/bag_items.gd
+++ scripts/bag_items.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 var item
+@export var shelf=false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if(shelf):
+		return
 	for child in get_children():
 		if not child.name=="Carrots":
 			child.freeze=true
--- scripts/player.gd
+++ scripts/player.gd
@@ -20,66 +20,18 @@ func _ready() -> void:
 	GameManager.hud=$HUD
 
 func _process(_delta: float) -> void:
-	$Area2D/CollisionShape2D.scale=Vector2(1/GameManager.camera.zoom.x, 1/GameManager.camera.zoom.y)
-	if(GameManager.carrots>0 and not "Carrots" in GameManager.inventory and len(GameManager.inventory) <= 4):
-		GameManager.inventory.append("Carrots")
-	$CanvasLayer/OverlayArm/Sprites/Square4/tape.visible=GameManager.playerTool=="tape"
-	$CanvasLayer/OverlayArm/Sprites/Square4/Square5.visible=GameManager.playerTool=="rag"
-	$CanvasLayer/SeedBag.visible=GameManager.playerTool=="seedBag"
-	$CanvasLayer/PlantBag.visible=GameManager.playerTool=="bag"
-	$CanvasLayer/OverlayArm/Sprites/Square4/wateringCan.visible=GameManager.playerTool=="wateringCan"
-	if(handHeldItem=="seeds" and Input.is_action_just_released("Click")):
-		GameManager.interactedItem.dropSeeds()
-		handHeldItem=""
-		$CanvasLayer/OverlayArm/Sprites/Square4.scale=Vector2(1, 1)
-	$"CanvasLayer/OverlayArm/IK Targets/TIP".global_position=get_viewport().get_mouse_position()
-	$Area2D.global_position=get_global_mouse_position()
-	$Helmet/PointLight2D.look_at(get_global_mouse_position())
-	$Helmet/PointLight2D.rotation+=PI/2
-	if(Input.is_action_just_pressed("Flashlight")):
-		GameManager.flashlightOn=!GameManager.flashlightOn
-	if(GameManager.playerEnergy<=0 and GameManager.flashlightOn):
-		GameManager.flashlightOn=false
-	$Helmet/PointLight2D.enabled=GameManager.flashlightOn
-	if(GameManager.playerSprinting):
-		speed=600
-	else:
-		speed=300
-	velocity = Input.get_vector("Left", "Right", "Up", "Down")
-	var healthMod = float(GameManager.health/100.0)
-	if(healthMod<.6):
-		healthMod=.6
-	velocity*=speed*healthMod
-	if(GameManager.playerMove):
-		move_and_slide()
+	perFrameUpdate()
+	seedCheck()
+	flashlight()
+	sprintCheck()
+	movement()
+	
+func _physics_process(delta: float) -> void:
+	#All of the fun interactions with moving stuff to and fro shelves
 	if(pickedUp == false and Input.is_action_just_pressed("Click") and pickable!=null):
 		canPickUp=false
 		if(bagOpen==false):
-			pickedUpType=str(pickable.get_child(-1).name)
-			$CanvasLayer/PlantBag.loadInventory()
-			if(len(GameManager.inventory)>4):
-				$CanvasLayer/PlantBag.get_node("full").play("full")
-				canPickUp=false
-				pickedUpType=""
-				pickable=null
-				print("NNNNNNNYYYYYYYYYYYLLLLLLLLLLLL")
-				return
-			if(pickable==null):
-				return
-			#var world_pos=pickable.global_position
-			pickable.freeze=true
-			pickedUp=true
-			pickedUpParent=pickable.get_parent()
-			#var oldPos = pickable.global_position
-			pickable.reparent($CanvasLayer/OverlayArm/Sprites/Square4)
-			#pickable.rotation+=PI/2
-			handHeldItem=pickedUpType.to_lower()
-			currentlyHeld=pickable
-			prints("ESTA UNA ", pickable)
-			for child in pickable.get_children():
-				child.scale*=GameManager.camera.zoom
-			#pickable.scale*=GameManager.camera.zoom
-			pickable.position=$CanvasLayer/OverlayArm/Sprites/Square4/Position.position
+			closedBagPickup()
 		else:
 			pickedUp=true
 			pickedUpParent=pickable.get_parent()
@@ -181,3 +133,70 @@ func _in_bag_exited(area: Area2D) -> void:
 	if($CanvasLayer/OverlayArm.modulate.a>=.9 and area.get_parent().visible and handHeldItem=="" and GameManager.playerTool=="bag" and bagOpen==true):
 		if(pickable==area.get_parent()):
 			pickable=null
+
+func seedCheck():
+	if(handHeldItem=="seeds" and Input.is_action_just_released("Click")):
+		GameManager.interactedItem.dropSeeds()
+		handHeldItem=""
+		$CanvasLayer/OverlayArm/Sprites/Square4.scale=Vector2(1, 1)
+		
+func perFrameUpdate():
+	$Area2D/CollisionShape2D.scale=Vector2(1/GameManager.camera.zoom.x, 1/GameManager.camera.zoom.y)
+	$CanvasLayer/OverlayArm/Sprites/Square4/tape.visible=GameManager.playerTool=="tape"
+	$CanvasLayer/OverlayArm/Sprites/Square4/Square5.visible=GameManager.playerTool=="rag"
+	$CanvasLayer/SeedBag.visible=GameManager.playerTool=="seedBag"
+	$CanvasLayer/PlantBag.visible=GameManager.playerTool=="bag"
+	$CanvasLayer/OverlayArm/Sprites/Square4/wateringCan.visible=GameManager.playerTool=="wateringCan"
+	$"CanvasLayer/OverlayArm/IK Targets/TIP".global_position=get_viewport().get_mouse_position()
+	$Area2D.global_position=get_global_mouse_position()
+	$Helmet/PointLight2D.look_at(get_global_mouse_position())
+	$Helmet/PointLight2D.rotation+=PI/2
+	
+func flashlight():
+	if(Input.is_action_just_pressed("Flashlight")):
+		GameManager.flashlightOn=!GameManager.flashlightOn
+	if(GameManager.playerEnergy<=0 and GameManager.flashlightOn):
+		GameManager.flashlightOn=false
+	$Helmet/PointLight2D.enabled=GameManager.flashlightOn
+	
+func sprintCheck():
+	if(GameManager.playerSprinting):
+		speed=600
+	else:
+		speed=300
+		
+func movement():
+	velocity = Input.get_vector("Left", "Right", "Up", "Down")
+	var healthMod = float(GameManager.health/100.0)
+	if(healthMod<.6):
+		healthMod=.6
+	velocity*=speed*healthMod
+	if(GameManager.playerMove):
+		move_and_slide()
+		
+func closedBagPickup():
+	pickedUpType=str(pickable.get_child(-1).name)
+	$CanvasLayer/PlantBag.loadInventory()
+	if(len(GameManager.inventory)>4):
+		$CanvasLayer/PlantBag.get_node("full").play("full")
+		canPickUp=false
+		pickedUpType=""
+		pickable=null
+		print("NNNNNNNYYYYYYYYYYYLLLLLLLLLLLL")
+		return
+	if(pickable==null):
+		return
+	#var world_pos=pickable.global_position
+	pickable.freeze=true
+	pickedUp=true
+	pickedUpParent=pickable.get_parent()
+	#var oldPos = pickable.global_position
+	pickable.reparent($CanvasLayer/OverlayArm/Sprites/Square4)
+	#pickable.rotation+=PI/2
+	handHeldItem=pickedUpType.to_lower()
+	currentlyHeld=pickable
+	prints("ESTA UNA ", pickable)
+	for child in pickable.get_children():
+		child.scale*=GameManager.camera.zoom
+	#pickable.scale*=GameManager.camera.zoom
+	pickable.position=$CanvasLayer/OverlayArm/Sprites/Square4/Position.position

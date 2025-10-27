344b41e52544622d8ab04b408a79377ce9104140
Taxalottle Syndicate <bthole41@gmail.com>
2025-10-27 18:41:03 -0400

    script cleaning

--- scripts/computer.gd
+++ scripts/computer.gd
@@ -1,18 +1,21 @@
 extends Node2D
 
-@export var startingText: String
+@export var textToDisplay: String
 var visibleCharacters:=0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$RichTextLabel.text=startingText
+	$RichTextLabel.text=textToDisplay
 	$RichTextLabel.visible_characters=0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	visibleCharacters+=delta*25
-	if(visibleCharacters>1):
-		visibleCharacters-=1
-		$RichTextLabel.visible_characters+=1
+	if($RichTextLabel.visible_ratio<1.0):
+		visibleCharacters+=delta*25
+		if(visibleCharacters>1):
+			visibleCharacters-=1
+			$RichTextLabel.visible_characters+=1
+	else:
+		$RichTextLabel.visible_ratio=1.0
 		

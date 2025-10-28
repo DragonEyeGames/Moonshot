65678c4634977146933574efb40ee59d4f176a3e
Taxalottle Syndicate <bthole41@gmail.com>
2025-10-28 18:20:53 -0400

    energy

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -15,7 +15,7 @@ var playerState: possibleStates=possibleStates.OUTSIDE
 
 var flashlightOn=false
 var playerEnergy:=100.0
-var inventory=[{"name": "CleanFilter", "count": 1}]
+var inventory=[]
 var selectedSlot=0
 var playerMove=true
 var dead=""
--- scripts/systemsController.gd
+++ scripts/systemsController.gd
@@ -2,19 +2,20 @@ extends ColorRect
 
 @export var fallingColor:Color
 @export var risingColor:Color
-var previousOxygen=0
 var update:=0.0
 var colorShowing:=false
 var oxygenState=0
 var waterState=0
+var powerState=0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	checkOxygen()
+	checkStats()
 
 
-func checkOxygen():
+func checkStats():
 	var previousOxygen=GameManager.baseOxygen
 	var previousWater=GameManager.baseWater
+	var previousEnergy=GameManager.basePower
 	await get_tree().create_timer(.5).timeout
 	if(GameManager.baseOxygen-previousOxygen<-1):
 		oxygenState=-1
@@ -22,14 +23,21 @@ func checkOxygen():
 		oxygenState=1
 	else:
 		oxygenState=0
+		
 	if(GameManager.baseWater-previousWater<-.001):
 		waterState=-1
 	elif(GameManager.baseWater-previousWater>.001):
-		print("rse")
 		waterState=1
 	else:
 		waterState=0
-	checkOxygen()
+		
+	if(GameManager.basePower-previousEnergy<-.05):
+		powerState=-1
+	elif(GameManager.basePower-previousEnergy>.05):
+		powerState=1
+	else:
+		powerState=0
+	checkStats()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	update+=delta
@@ -38,6 +46,7 @@ func _process(delta: float) -> void:
 		colorShowing=!colorShowing
 	oxygen()
 	water()
+	energy()
 	
 
 func oxygen():
@@ -53,7 +62,6 @@ func oxygen():
 		var showingColor="[color=" + str(rising)
 		oxygenSuffix=showingColor + "]Rising"
 	$Oxygen.text="Oxygen . . . . . . . . . "+ oxygenSuffix+ " " + str(round((GameManager.baseOxygen/(GameManager.baseOxygen+GameManager.baseCarbon))*10000)/100)+"%"
-	previousOxygen=GameManager.baseOxygen
 	
 func water():
 	var rising=risingColor.to_html(true)
@@ -68,4 +76,17 @@ func water():
 		var showingColor="[color=" + str(rising)
 		suffix=showingColor + "]Rising"
 	$Water.text="Water . . . . . . . . . . "+ suffix+ " " + str(round(GameManager.baseWater*100)/400)+"Ml"
-	previousOxygen=GameManager.baseOxygen
+	
+func energy():
+	var rising=risingColor.to_html(true)
+	var falling=fallingColor.to_html(true)
+	var suffix:="[color=white]Stable"
+	if(powerState==-1):
+		var showingColor=""
+		if(colorShowing):
+			showingColor = "[color=" + str(falling) + "]"
+		suffix=showingColor + "Falling"
+	elif(powerState==1):
+		var showingColor="[color=" + str(rising)
+		suffix=showingColor + "]Rising"
+	$Power.text="Power . . . . . . . . . . "+ suffix+ " " + str(round(GameManager.basePower*10)/10)+"KwH"

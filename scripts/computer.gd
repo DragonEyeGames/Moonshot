b93e79b457b3e126e8e8b57c0b3063ff6f2c8687
Taxalottle Syndicate <bthole41@gmail.com>
2025-10-27 21:32:57 -0400

    computer interface start

--- scripts/computer.gd
+++ scripts/computer.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
-@export var textToDisplay: String
+@export var textToDisplay: Array[String]
+var displayedText=-1
 var visibleCharacters:=0.0
+var entered=false
+
+var zoomed=false
+var canZoom=true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$RichTextLabel.text=textToDisplay
 	$RichTextLabel.visible_characters=0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+#	prints(entered, zoomed, canZoom)
 	if($RichTextLabel.visible_ratio<1.0):
 		visibleCharacters+=delta*25
 		if(visibleCharacters>1):
@@ -18,4 +23,46 @@ func _process(delta: float) -> void:
 			$RichTextLabel.visible_characters+=1
 	else:
 		$RichTextLabel.visible_ratio=1.0
+	if(entered and not zoomed and canZoom and Input.is_action_just_pressed("Interact")):
+		zoom()
+		updateText()
+	elif(entered and zoomed and canZoom and Input.is_action_just_pressed("Interact")):
+		unzoom()
 		
+	if(Input.is_action_just_pressed("Click") and $RichTextLabel.visible_ratio>=1.0 and entered):
+		updateText()
+	elif(Input.is_action_just_pressed("Click") and $RichTextLabel.visible_ratio<1.0 and entered):
+		$RichTextLabel.visible_ratio=1.0
+		
+func updateText():
+	displayedText+=1
+	$RichTextLabel.text=textToDisplay[displayedText]
+	$RichTextLabel.visible_characters=0
+	print($RichTextLabel.text)
+	$RichTextLabel.visible_ratio=0
+
+func _player_entered(_body: Node2D) -> void:
+	entered=true
+
+
+func _player_exited(_body: Node2D) -> void:
+	entered=false
+	
+func zoom():
+	GameManager.zoomCamera($ZoomPoint, 5)
+	zoomed=true
+	canZoom=false
+	GameManager.playerMove=false
+	GameManager.flashlightOn=false
+	GameManager.playerAnimator.play("fade")
+	await get_tree().create_timer(1.1).timeout
+	canZoom=true
+
+func unzoom():
+	GameManager.unzoomCamera()
+	zoomed=false
+	canZoom=false
+	GameManager.playerMove=true
+	GameManager.playerAnimator.play("appear")
+	await get_tree().create_timer(1.1).timeout
+	canZoom=true

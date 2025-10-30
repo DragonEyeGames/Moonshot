ae1cd4174118846bbc28b55c072962bc92a24620
Taxalottle Syndicate <bthole41@gmail.com>
2025-10-29 20:30:29 -0400

    New Items Pt 1
    added in new textures for items, gave the jug semi realistic water

---
+++ scripts/jug.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	$Jug2/Jug3.global_rotation=0
